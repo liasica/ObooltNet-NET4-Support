3eddc580e5d8f39a321ad32cbad97682dbfed93f
liasica <magicrolan@qq.com>
2015-08-08 08:54:32 +0800

    Fix ObooltNet for NET4
    Fix ObooltNet for NET4, add test.

---
+++ SocketTest/SocketTest/Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ObooltNet;
+
+namespace SocketTest
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            NetConnection client = new NetConnection();
+            client.OnConnect += Client_OnConnect;
+            client.OnDataReceived += Client_OnDataReceived;
+            client.OnDisconnect += Client_OnDisconnect;
+
+            client.Connect("localhost", 55555);
+
+            while (true)
+            {
+                Console.WriteLine(">>>");
+                client.Send(Encoding.UTF8.GetBytes(Console.ReadLine()));
+            }
+        }
+
+        private static void Client_OnDataReceived(object sender, NetConnection connection, byte[] e)
+        {
+            Console.WriteLine("Message from server " + Encoding.UTF8.GetString(e));
+        }
+
+        private static void Client_OnDisconnect(object sender, NetConnection connection)
+        {
+            Console.WriteLine("Disconnected from " + connection.RemoteEndPoint);
+        }
+
+        private static void Client_OnConnect(object sender, NetConnection connection)
+        {
+            Console.WriteLine("Connection to " + connection.RemoteEndPoint);
+        }
+    }
+}
---
+++ SocketTest/SocketTest_server/Server.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ObooltNet;
+
+namespace SocketTest_server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            NetConnection server = new NetConnection();
+            server.OnConnect += Server_OnConnect;
+            server.OnDataReceived += Server_OnDataReceived;
+            server.OnDisconnect += Server_OnDisconnect;
+
+            // server start
+            server.Start(55555);
+
+            while (true) ;
+        }
+
+        private static void Server_OnDisconnect(object sender, NetConnection connection)
+        {
+            Console.WriteLine("Disconnected from " + connection.RemoteEndPoint);
+        }
+
+        private static void Server_OnDataReceived(object sender, NetConnection connection, byte[] e)
+        {
+            Console.WriteLine("Message from " + connection.RemoteEndPoint + " : " + Encoding.UTF8.GetString(e));
+        }
+
+        private static void Server_OnConnect(object sender, NetConnection connection)
+        {
+            Console.WriteLine("Connection from " + connection.RemoteEndPoint);
+        }
+    }
+}
